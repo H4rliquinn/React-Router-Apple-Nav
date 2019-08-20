bc030e4f7536c05b7c3a7463ea172e41b87d20a8
Jason Sonnichsen <Harliquinn@gmail.com>
2019-08-20 18:59:49 -0400

    Subnav should be working, but it doesn't show up right now

--- apple-nav/src/components/SubNav.js
+++ apple-nav/src/components/SubNav.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import {NavLink, Route} from 'react-router-dom';
 
-const subs={
-    mac:["Stuff"],
-    ipad:["Stuff"],
-    iphone:["Stuff"],
-    watch:["Stuff"],
-    tv:["Stuff"],
-    music:["Stuff"],
-    support:["Stuff"]
-};
+
 
 function SubNav(props){
+
+    const subs=[
+        {id:"mac",mac:["Stuff","Other Stuff"]},
+        {id:"ipad",ipad:["Stuff","Other Stuff"]},
+        {id:"iphone",iphone:["Stuff","Other Stuff"]},
+        {id:"watch",watch:["Stuff"]},
+        {id:"tv",tv:["Stuff"]},
+        {id:"music",music:["Stuff"]},
+        {id:"support",support:["Stuff"]}
+    ];
+
     // console.log(props.match.params.tech);
+    const items=subs.find((item)=>{
+        return props.match.params.tech===String(item.id)
+    });
+    // console.log(items);
     return (
         <div className="subNav">
-            <NavLink to="/">Stuff and shit</NavLink>
+            <NavLink className="subNavLink" to="/"><h2>Start</h2></NavLink>
+            {items[props.match.params.tech].forEach((item)=>{
+                console.log(item);
+                return <NavLink className="subNavLink" to="/"><h2>{item}</h2></NavLink>
+            })}
+            <NavLink className="subNavLink" to="/"><h2>End</h2></NavLink>
+
         </div>
     )
 }
